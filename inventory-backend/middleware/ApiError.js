d93f2222834ace9dc75152c4baaa9b482d1548f4
moxhamusic <moxhamusic@gmail.com>
2026-08-09 02:48:29 +0530

    Delete inventory-backend directory

--- inventory-backend/middleware/ApiError.js
+++
@@ -1,9 +0,0 @@
-// Custom error class to carry an HTTP status code alongside the message
-class ApiError extends Error {
-  constructor(statusCode, message) {
-    super(message);
-    this.statusCode = statusCode;
-  }
-}
-
-module.exports = ApiError;
